3f8aea07614df317bbe375ec7d776760b7ab3b73
spencermv101 <spencermv@appstate.edu>
2024-12-10 15:38:02 -0500

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Appalachian Trail Murders from 1974-2021
+The Appalachian Trail (AT) is one of the a iconic and well-traveled hiking route in the United States, spanning over 2,190 miles across 14 states. It is a spot for nature enthusiasts, offering beautiful views, diverse ecosystems, and a chance to disconnect from urban life. However, even this serene trail has not been immune to tragic events.
+
+This map provides a geographic representation of all recorded murders that have occurred on or in close proximity to the Appalachian Trail. By visualizing these incidents, the map aims to shed light on the patterns and locations of these rare but impactful tragedies. The purpose is not only to document these events but also to prompt discussions on safety measures and awareness for hikers and trail managers.
+
+Each location includes relevant information, such as the date, victim(s) image, and a brief context of the incident. By integrating these details into an interactive visual format, the map offers a deeper understanding of safety challenges faced on long-distance trails like the AT.
+
+
+
+--
+
+## Major Functions
+- Easy-to-use interface 
+- Zoom and Pan over the map 
+- Click on markers 
+- Image popup
+- Functioning base map
+- Detailed labels
+- Description box
+- Link to safety tips 
+- Precise location of events on the A.T.
+
+---
+
+## Libraries
+- Cloudflare
+- Googleapis
+- Leaflet Providers
+- JQuery
+
+## Data Sources
+- greenbelly.co
+- Appalachiantrailconservancy.org
+- geoJSON.io
+- QGIS
+- Appalachian Trail Centerline - ArcGIS - SHP
+- GPX.studio 
---
+++ route.js
@@ -0,0 +1,225 @@
+var data = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim - Meredith Emerson, Murderer-Gary Micheal Hilton",
+          "time": 2008,
+          "place": "Blood Mountain, GA ",
+          "description": "Gary Michael Hilton, a 61-year-old man, briefly hiked with 24-year-old Meredith Emerson, who was accompanied by her dog. Hilton later attacked, kidnapped, and held her captive for three days before murdering her with a car jack handle. A trained martial artist, Emerson fiercely resisted, prompting Hilton to remark, \"She wouldn’t stop fighting\" (Licavoli, Greenbelly). Initially, Hilton targeted her for money, but his actions were ultimately driven by mental instability and sociopathic tendencies.",
+          "image": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/msnbc/Components/Photo_StoryLevel/080323/080323-emrson-hmed-3p.jpg",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -83.93640518188477,
+            34.73872073120363,
+            1341.49
+          ],
+          "type": "Point"
+        },
+        "id": 0
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim-Joel Polson, Murderer-Ralph Howard Fox",
+          "time": 1974,
+          "place": "Low Gap Shelter, GA ",
+          "description": "The first recorded murder on the A.T. Joel Polson was spending the night at Low Gap Shelter with a woman named Margaret McFaddin. The next morning the murderer Ralph Howard Fox shot Joel Polson and kidnapped Margaret McFaddin for three days then released her. The motive of Ralph Howard Fox was theft and he told Margaret it was too bad they didn't meet at different times because he said I would have liked you. ",
+          "image": "https://cdn.outsideonline.com/wp-content/uploads/2018/10/26/joel-polson-with-camera.jpg?width=100",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -83.82448196411133,
+            34.77630581458913,
+            904.31
+          ],
+          "type": "Point"
+        },
+        "id": 1
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim-Janice Balza, Murderer-Paul Bigley",
+          "time": 1975,
+          "place": "Vandeventer Shelter, TN",
+          "description": "Sitting at the campfire Janice Balza, 22 was murdered by Paul Bigley, 51 with a hatchet. Paul Bigley desired her backpack. Paul Bigley was a former mental health patient who was mentally unstable. The Vandeventer shelter is now said to be haunted by Balza’s ghost. ",
+          "image": "https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,q_auto,w_720/62c746d8004665001da62c40.jpg?width=100",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -82.05877304077148,
+            36.3670469739116,
+            1093.38
+          ],
+          "type": "Point"
+        },
+        "id": 2
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim-Ronald Sanchez, Murderer-James Louis Jordan",
+          "description": "Ronald Sanchez, 43 a father and husband was killed by James Louis Jordan, 32 who has pleaded nonguilty by reasons of insanity. James Louis Jordan stabbed Ron Sanchez to death and also wounded Kirby Morrill, 30 along the trail. The motive was that Jordan suffers from schizoaffective disorder and psychosis acute symptoms. ",
+          "place": "Wythe County",
+          "image": "https://photos.thetrek.co/wp-content/uploads/2019/05/15112037/ronald-sanchez-700x933.jpg?width=100",
+          "time": 2019,
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -81.42238140106201,
+            36.80945649934735,
+            990.26
+          ],
+          "type": "Point"
+        },
+        "id": 3
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim-Robert Mountford JR and Laura Susan Ramsay, Murderer-Randall Lee Smith",
+          "time": 1981,
+          "place": "Wapiti Shelter, VA ",
+          "description": "Robert Mountford JR and Laura Susan Ramsay, 27 were hiking on the A.T. to raise money and awareness for troubled teens. They were killed by Randall Lee Smith at the Wapiti shelter. Robert Mountford JR was shot in the head and Laura Susan Ramsay was struck with iron and then stabled repeatedly. The motive is still unsolved. Years later he attempted murder in the same spot but both men survived. ",
+          "image": "https://images.findagrave.com/photos/2017/328/180568197_1511638855.jpg?width=100",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -80.82366943359375,
+            37.224109260944196,
+            813.61
+          ],
+          "type": "Point"
+        },
+        "id": 4
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim- Scott Lill, Murderer-Unsolved",
+          "time": 2011,
+          "place": "Cow Camp Gap Shelter, VA ",
+          "image": "https://bloximages.newyork1.vip.townnews.com/newsadvance.com/content/tncms/assets/v3/editorial/a/29/a29c7172-beea-5038-8475-4ac28993ef40/5122c16507ee9.image.jpg?width=100",
+          "description": "Lilly, 30 was a SOBO hiker that was murdered near the Cow Camp Gap Shelter. The cause of murder was asphyxia caused by suffocation. Lilly’s death is ruled a homicide. Since found on federal lands the FBI investigated but found no clues, they do not believe the motive was robbery. Lilly's death is a mystery still today.  ",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -79.21279907226562,
+            37.74482679633051,
+            975.88
+          ],
+          "type": "Point"
+        },
+        "id": 5
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim-Julianne Williams and Lollie Winans",
+          "time": 1996,
+          "place": "Shenandoah National Park, VA",
+          "description": "The double-murder though technically not on the A.T was in the Shenandoah National Park at a Virgina campsite. Julianne Williams, 24, and Lollie Winans, 26 were murdered by a serial rapist. The killer was not identified for three decades until they linked it to his DNA. Walter Leo Jackson Sr killed the two women and did it by slashing their necks and raping them. ",
+         "image": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2024-06/240620-Laura-Lollie-Winans-Julianne-Julie-Williams-ew-442p-5ab957.jpg?width=100",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -78.43684136867523,
+            38.51779669695656,
+            1079.47
+          ],
+          "type": "Point"
+        },
+        "id": 6
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim-Rebecca Wight, Murderer- Stephan Ray Carr",
+          "description": "Rebecca Wight, 29, and her partner Claudia Brenner, 31, were hiking on the Rocky Knob loop trail in Michaux State Forest when they encountered Stephen Roy Carr, who asked for a cigarette. Later that evening, the couple set up camp by a secluded creek. While they were sharing an intimate moment, Carr fired eight shots, critically injuring Brenner and killing Wight. The attack was motivated by Carr's hatred of their sexual orientation, making it a violent hate crime.",
+          "place": "Michaux State Forest, PA ",
+          "time": 1988,
+          "image": "https://images.findagrave.com/photos/2021/245/95691242_cad3bd56-5ea1-45a8-b9b2-e4943a1ef620.jpeg?width=100",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -77.44159698486328,
+            39.97817244470628,
+            569.79
+          ],
+          "type": "Point"
+        },
+        "id": 7
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victim-Geoffrey Hood and Molly Larue, Murderer-Paul David Crews ",
+          "time": 1990,
+          "place": "Thelma Marks Shelter,PA",
+          "description": "Geoffrey Hood, 26, and Molly LaRue, 25, were thru-hiking the Appalachian Trail when they stopped for the night at Thelma Marks Shelter. There, they were brutally attacked by Paul David Crews, a fugitive on the run. Hood was shot three times, while LaRue was tied up, assaulted, and stabbed eight times. Crews, a manic-depressive with a history of drug abuse, carried out the heinous double homicide without provocation, leaving a lasting scar on the hiking community.",
+          "image": "https://cdn.outsideonline.com/wp-content/uploads/2015/08/25/molly-larue-geoff-hood-appalachian-trail_h.jpg?width=100",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -77.06739664077759,
+            40.36368073369715,
+            343.52
+          ],
+          "type": "Point"
+        },
+        "id": 8
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "description": "Louise Chaput, 61 was found a short distance off the Glen Boulder Trail on Thanksgiving day with multiple stab wounds. The murder is unsolved and her family struggles to live without closure. Louise was only in NH for a short hike and drove from Canada to pursue it. Her family speaks French and has a hard time dealing with an American case. ",
+          "name": "Victim-Louise Chaput ,Murderer-Unsolved",
+          "time": 2001,
+          "image": "https://img1.wsimg.com/isteam/ip/40109219-e2c6-42c7-9a59-530e026efdd0/26114772-26114772.jpg?width=100",
+          "place": "Lost Pond, NH",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            -71.25067279809076,
+            44.25079426055713,
+            633.12
+          ],
+          "type": "Point"
+        },
+        "id": 9
+      }
+    ]
+  }
